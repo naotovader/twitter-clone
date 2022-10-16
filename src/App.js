8bdbc15749a4f54e243438de77f11c638464faaf
naotovader <gallardo.st62@gmail.com>
2022-10-16 10:03:51 +0900

    add twitter icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
+    // BEM
+    <div className="app">
       <h1>Hey Clever Programmers... Let's build a Twitter Clone 🚀</h1>
+
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Feed */}
+
+      {/* Widgets */}
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+function Sidebar() {
+  return (
+    <div className="Sidebar">
+      {/* Twitter icon */}
+      <TwitterIcon />
+
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+
+      {/* Button -> Tweet */}
+    </div>
+  );
+}
+
+export default Sidebar;
